529b50f0daa3a739ee495c57b3f25e5ac689a3ec
bummings <sup@fauxkno.com>
2018-08-15 01:44:37 -0400

    almost

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,38 @@
-var redButton = document.querySelector('.redButton');
 var nav = document.querySelector('.nav');
+var headlineMobile = document.querySelector('.headlineMobile__menu');
 
-redButton.addEventListener('click', function(){
-  nav.classList.toggle('red');
+var hideButton = document.querySelector('.hideButton');
+
+var burger = document.getElementById('burger');
+
+burger.addEventListener('click', function () {
+  //now display a motherfuckin menu you idiot
+  headlineMobile.classList.toggle('hidden');
 });
 
 
 $(document).ready(function () {
   var $obj = $('.nav');
+  var top = $obj.offset().top - parseFloat($obj.css('marginTop').replace(/auto/, 0));
+
+  $(window).scroll(function (event) {
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+
+    // whether that's below the form
+    if (y >= top) {
+      // if so, ad the fixed class
+      $obj.addClass('fixed');
+    } else {
+      // otherwise remove it
+      $obj.removeClass('fixed');
+    }
+  });
+});
+
+
+$(document).ready(function () {
+  var $obj = $('.headlineMobile');
   var top = $obj.offset().top - parseFloat($obj.css('marginTop').replace(/auto/, 0));
 
   $(window).scroll(function (event) {
@@ -25,4 +50,7 @@ $(document).ready(function () {
     }
   });
 });
+
+
+
 
